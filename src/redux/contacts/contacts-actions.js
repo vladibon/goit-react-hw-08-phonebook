3e526104cbada007ae2f7275d63d8fb9e1d3819f
Vladimir Bondar <vladi.bondar@gmail.com>
2021-12-08 12:12:53 +0200

    fix

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -7,4 +7,4 @@ export const addContact = createAction('contacts/add', contact => ({
   payload: { id: nanoid(), ...contact },
 }));
 export const deleteContact = createAction('contacts/delete');
-// export const changeFilter = createAction('contacts/changeFilter');
+export const changeFilter = createAction('contacts/changeFilter');
