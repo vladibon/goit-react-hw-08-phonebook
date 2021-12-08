257ede529745a5d3d01cafbf04c9c6aab58678e3
Vladimir Bondar <vladi.bondar@gmail.com>
2021-12-08 21:39:24 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Filter from 'components/Filter';
 import ContactList from 'components/ContactList';
 
 function App() {
-  const contacts = useSelector(({ contacts }) => contacts.items);
+  const { items } = useSelector(({ contacts }) => contacts);
 
   return (
     <main>
@@ -16,7 +16,7 @@ function App() {
         <h2>Contacts</h2>
         <Filter labelName='Find contacts by name' />
 
-        {contacts.length > 0 && <ContactList />}
+        {items.length > 0 && <ContactList />}
       </Container>
     </main>
   );
